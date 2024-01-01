6a7c4276bcb84e00051dfa0805a7479c64a73d60
gorodnyuk <sergeygorodnyuk@yandex.ru>
2024-01-01 17:08:34 +0300

    Доработан обработчик

--- src/main/java/ru/gorodnyuk/command/CommandFactory.java
+++ src/main/java/ru/gorodnyuk/command/CommandFactory.java
@@ -2,16 +2,12 @@ package ru.gorodnyuk.command;
 
 public class CommandFactory {
 
-    public static CommandExecutor getCommandExecutor() {
-        return new CommandExecutor();
-    }
-
     public static CommandParser getCommandParser() {
         return new CommandParser();
     }
 
     public static CommandProcessor getCommandProcessor() {
-        return new CommandProcessor(getCommandExecutor());
+        return new CommandProcessor();
     }
 
     public static CommandValidator getCommandValidator() {
--- src/main/java/ru/gorodnyuk/command/CommandProcessor.java
+++ src/main/java/ru/gorodnyuk/command/CommandProcessor.java
@@ -9,12 +9,6 @@ public class CommandProcessor {
     private static final int DEFAULT_START_DELAY_SECONDS = 60;
     private static final int DEFAULT_EXECUTION_TIMEOUT_SECONDS = 3600;
 
-    private final CommandExecutor commandExecutor;
-
-    public CommandProcessor(CommandExecutor commandExecutor) {
-        this.commandExecutor = commandExecutor;
-    }
-
     public void process(Map<String, String> commandsMap) {
         long startTimeout = getExecutionTime(commandsMap.get("-d"), DEFAULT_START_DELAY_SECONDS);
         long executionTimeout = getExecutionTime(commandsMap.get("-t"), DEFAULT_EXECUTION_TIMEOUT_SECONDS);
